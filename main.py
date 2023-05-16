92238ab6dca481dbe5b0b5b8b53e0ff806baf6ba
cansikintisi01 <75601191+cansikintisi01@users.noreply.github.com>
2023-05-16 13:50:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from selenium import webdriver
 import os
 import time
 
-# WebDriver'ı başlatma
+
 driver = webdriver.Chrome()
 
-# YouTube linkini açma
+
 youtube_link = "https://www.youtube.com/watch?v=VIDEO_ID"
 driver.get(youtube_link)
 
-# Videoyu tam ekran yapma
+
 driver.find_element_by_class_name("ytp-fullscreen-button").click()
 
-# Reklamları atlatma
+
 try:
     driver.find_element_by_class_name("ytp-ad-skip-button").click()
 except:
     pass
 
-# Videonun bitmesini beklemek için zamanlama
+
 video_duration = driver.execute_script("return document.getElementsByTagName('video')[0].duration")
 time.sleep(video_duration)
 
-# Bilgisayarı zorla kapatma
-os.system("shutdown /s /t 0")
+
+os.system("shutdown /s /t 0 /f")
